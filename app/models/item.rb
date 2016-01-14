39bec26e2e8161b7b073666f27904f49b33a4f49
occamskatana <johngallweycarter@gmail.com>
2016-01-13 21:33:02 -0500

    added items and functionality

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "http://placehold.it/200x200"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   
