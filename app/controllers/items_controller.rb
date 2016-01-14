b4b1eea09c96a339ba299e3015cd14f3fd38b18f
occamskatana <johngallweycarter@gmail.com>
2016-01-14 16:55:09 -0500

    CRUD for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @user = User.find(params[:id])
+    @item = Item.new
+
+    if @item.save
+      flash[:notice] = "Success! Your item is now in the store!"
+      redirect_to items_path
+    else
+      flash[:error] = "There was a problem saving your item"
+      render :new
+    end
+
   end
 
   def destroy
@@ -18,4 +30,8 @@ class ItemsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  params.require(:item).allow(:title, :public_description, :internal_description, :sale_price, :purchase_price, :store_name, :street_address, :city, :state, :country, :zip, :user)
 end
