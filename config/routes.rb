7aa7142fbae07cb204749e7a36d5ff0d2c5ee276
occamskatana <johngallweycarter@gmail.com>
2016-01-15 00:01:06 -0500

    user show view progress and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+ 
+
   resources :items
 
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
-  
+  resources :users, only: [:show]
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
